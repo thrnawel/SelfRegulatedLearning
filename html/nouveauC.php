692a644d45ee933654759d3fad93ea5cb6bcb0f9
Ikram Benouhalima <benouhalima.ikram.g1@gmail.com>
2020-06-24 16:05:23 +0100

    créer compte

--- html/nouveauC.php
+++ html/nouveauC.php
@@ -1,31 +1,17 @@
-<?php  
+<?php
+$bdd = new PDO('mysql:host=localhost;dbname=srl_tool;', 'root', '');
+if(isset($_POST['forminscription']))
+{
+  if (!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['mail']) AND !empty($_POST['mdp']) ) 
+  {
+    echo "ok";
+  }
+  else 
+  {
+    e;
+  }
 
-$conn=mysqli_connect('localhost','root','','srl_tool') or die(mysqli_error());
-
-
-$NOM=$_POST['nom'];
-$PRE=$_POST['pre'];
-$EML=$_POST['eml'];
-$PWD=$_POST['pwd'];
-/*
-$SUBMIT=$_POST['submit'];
-if(isset($SUBMIT)){
-      if(empty($NOM))
-         $message='<div class="erreur">Nom laissé vide.</div>'; 
-      elseif(empty($PRE))
-         $message='<div class="erreur">Prénom laissé vide.</div>';
-      elseif(empty($EML))
-         $message='<div class="erreur">Email laissé vide.</div>';
-      elseif(empty($PWD))
-         $message='<div class="erreur">Mot de passe laissé vide.</div>';
-     }
-     else {
-*/
-$req="INSERT INTO apprenant (nomApprenant,prenomApprenant,email,mot_de_passe) VALUES ('$NOM','$PRE','$EML','$PWD')";
-
-$res=mysqli_query($conn,$req);
-
-
+}
 ?>
 
 <!DOCTYPE html>
@@ -43,7 +29,7 @@ $res=mysqli_query($conn,$req);
   <link href="../assets/css/font_style.css" rel="stylesheet" />
   <link href="../assets/css/icon_style.css" rel="stylesheet" />
   <link href="../assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
-   
+  
 </head>
 
 <body style="background-color: rgb(255, 255, 255);">
@@ -51,20 +37,20 @@ $res=mysqli_query($conn,$req);
       <div class="row" style="height: 100vh; width: auto;">
           <div class="col-7" style="background-color: rgba(241, 225, 243, 0.438);"></div>
           <div class="col">
-            <form class="border border-light p-5 mt-3" method="POST" >
+            <form class="border border-light p-5 mt-3">
                 <img class="img mx-auto d-block" src="../assets/img/new.png" />
                 <p class="h3 mb-4 text-center">Créer un compte</p>
             
                 <div class="row">
-                    <div class="col"><input type="text" class="form-control mb-4" name="nom" placeholder="Nom"></div>
-                    <div class="col"><input type="text" class="form-control mb-4" name="pre" placeholder="Prénom"></div>
+                    <div class="col"><input type="text" class="form-control mb-4" placeholder="Nom" id='nom' name="nom"></div>
+                    <div class="col"><input type="text" class="form-control mb-4" placeholder="Prénom" id='prenom' name="prenom"></div>
                 </div>
 
-                <input type="email" class="form-control mb-4" name="eml" placeholder="E-mail">
+                <input type="email" class="form-control mb-4" placeholder="E-mail"  id="mail" name="mail">
 
-                <input type="password" class="form-control" name="pwd" placeholder="Mot de passe">
+                <input type="password" class="form-control" placeholder="Mot de passe" id="mdp" name="mdp">
             
-                <button class="btn btn-primary btn-block my-4" type="submit" name="submit">S'enregistrer</button>
+                <button class="btn btn-primary btn-block my-4" name="forminscription" type="submit">S'enregistrer</button>
 
                 <div class="text-center">
                     <p>Vous avez un compte?
@@ -72,40 +58,6 @@ $res=mysqli_query($conn,$req);
                     </p>
                 </div>
             </form>
-           <!-- <php  
-if(isset($_POST["submit"])){  
-if(!empty($_POST['name']) && !empty($_POST['surname']) && !empty($_POST['email']) && !empty($_POST['password'])) {  
-    $user=$_POST['name'];  
-    $surname=$_POST['surname'];  
-    $email=$_POST['email']; 
-    $pass=$_POST['password']; 
-    $con=mysql_connect('localhost','root','') or die(mysql_error());  
-    mysql_select_db('srl_tool') or die("cannot select DB");  
-  
-    $query=mysql_query("SELECT * FROM Apprenant WHERE nameApprenant='".$user."'");  
-    $numrows=mysql_num_rows($query);  
-    if($numrows==0)  
-    {  
-    $sql="INSERT INTO Apprenant(nomApprenant,prenomApprenant,email,mot_de_passe,) VALUES('','$user','$surname','$email','$pass')";  
-   
-    $result=mysql_query($sql);  
-        if($result){  
-    echo "Account Successfully Created";  
-    } else {  
-    echo "Failure!";  
-    }  
-  
-    } else {  
-    echo "That username already exists! Please try again with another.";  
-    }  
-  
-} else {  
-    echo "All fields are required!";  
-}  
-}  
-?>  
-
--->
           </div>
       </div>
   </div>
