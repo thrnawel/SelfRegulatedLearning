0f8f1c6a880a8f2c8d33704b285fb4ad4225e4b9
Ikram Benouhalima <benouhalima.ikram.g1@gmail.com>
2020-06-26 15:39:55 +0100

    NouveauCompte

--- php/nouveauC.php
+++ php/nouveauC.php
@@ -1,1 +1,28 @@
-ok =D
+<?php  
+
+$conn=mysqli_connect('localhost','root','','srl_tool') or die (mysqli_error());
+
+
+$NOM=$_POST['nom'];
+$PRE=$_POST['pre'];
+$EML=$_POST['eml'];
+$PWD=($_POST['pwd']);
+
+if(isset($_POST['submit'])){
+      if(!empty($NOM) && !empty($PRE) && !empty($EML) && !empty($PWD)){
+        $req="INSERT INTO apprenant (nomApprenant,prenomApprenant,email,mot_de_passe) VALUES ('$NOM','$PRE','$EML','$PWD')";
+
+        $res=mysqli_query($conn,$req);
+
+      }
+      else
+      {
+        echo "Tous les champs doivent être complétés !"; 
+      }
+       
+     }
+    
+
+
+
+?>
